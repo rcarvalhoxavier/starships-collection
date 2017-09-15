3f07c649da2f1c1e367271bb70badd869b782a84
Rodrigo Xavier <rcarvalhoxavier@gmail.com>
2017-09-14 21:24:01 -0300

    Estruturando o projeto

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+const express    = require('express');
+const bodyParser = require('body-parser');
+//const db         = require('./app/models');
+const fs     = require('fs');
+const yaml   = require('js-yaml');
+const appRoot   = require('app-root-path');
+const app        = express();
+
+
+const env = process.env.NODE_ENV || 'local';
+const config = yaml.safeLoad(fs.readFileSync(`${appRoot}/config/config_${env}.yaml`, 'utf8'));
+
+app.set('view engine', 'html');
+app.set('views', 'public');
+app.set('port', config.api.port);
+
+// init server
+app.listen(config.api.port, () => {
+  console.log(`listening on port ${config.api.port}`); // eslint-disable-line no-console
+});
+
+
 module.exports = app; // for testing
 
-var config = {
+var configurationSwagger = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
+SwaggerExpress.create(configurationSwagger, function(err, swaggerExpress) {
   if (err) { throw err; }
-
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
+
