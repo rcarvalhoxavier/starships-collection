0fb9273ff1aec372963fb8ff960a0550b482163d
Rodrigo Xavier <rcarvalhoxavier@gmail.com>
2017-10-26 02:28:00 -0200

    implementado swagger-ui e separado os yaml

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-const express    = require('express');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+const express = require('express');
 const bodyParser = require('body-parser');
-const db         = require('./api/models');
-const fs     = require('fs');
-const yaml   = require('js-yaml');
-const appRoot   = require('app-root-path');
-const app        = express();
+const db = require('./api/models');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const appRoot = require('app-root-path');
+const app = express();
 
 
 const env = process.env.NODE_ENV || 'local';
 const config = yaml.safeLoad(fs.readFileSync(`${appRoot}/config/config_${env}.yaml`, 'utf8'));
 const port = config.api.port || process.env.PORT;
 console.log(env);
-console.log("port "+port);
+console.log("port " + port);
 
 app.set('view engine', 'html');
 app.set('views', 'public');
 app.set('port', port);
 
+app.use("/paths", express.static(`${appRoot}/api/swagger/paths`));
+app.use("/models", express.static(`${appRoot}/api/swagger/models`));
+
 // database
-db.sequelize.sync({ force : config.db.wipe }).then(() => {
+db.sequelize.sync({ force: config.db.wipe }).then(() => {
   console.log('Database synced' +  // eslint-disable-line no-console
     `${config.db.wipe ? ' - data it\'s wiped & schema recreated' : ''}`);
 });
@@ -32,16 +36,22 @@ app.listen(port, () => {
 });
 
 
+
+
 module.exports = app; // for testing
 
 var configurationSwagger = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(configurationSwagger, function(err, swaggerExpress) {
+SwaggerExpress.create(configurationSwagger, function (err, swaggerExpress) {
   if (err) { throw err; }
+  // Add swagger-ui (This must be before swaggerExpress.register)
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
   // install middleware
   swaggerExpress.register(app);
 
 });
+
+
 
